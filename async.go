767b7d4c8845413253a85cfa66bac1722ab0fd72
Egon Elbre <egonelbre@gmail.com>
2014-06-11 17:32:14 +0300

    Added async.All and async.Spawn functions.

---
+++ async.go
@@ -0,0 +1,55 @@
+package async
+
+import "sync/atomic"
+
+type Result struct {
+	Done  <-chan struct{}
+	Error <-chan error
+}
+
+// All starts all functions concurrently
+// if any error occurs it will be sent to the Error channel
+// after all functions have terminated the Done channel will get a single value
+func All(fns ...func() error) Result {
+	done := make(chan struct{}, 1)
+	errs := make(chan error, len(fns))
+
+	waiting := int32(len(fns))
+
+	for _, fn := range fns {
+		go func(fn func() error) {
+			if err := fn(); err != nil {
+				errs <- err
+			}
+
+			if atomic.AddInt32(&waiting, -1) == 0 {
+				done <- struct{}{}
+				close(errs)
+				close(done)
+			}
+		}(fn)
+	}
+
+	if len(fns) == 0 {
+		done <- struct{}{}
+		close(errs)
+		close(done)
+	}
+
+	return Result{done, errs}
+}
+
+// Spawns N routines, after each completes runs all whendone functions
+func Spawn(N int32, fn func(id int), whendone ...func()) {
+	waiting := N
+	for k := int32(0); k < N; k += 1 {
+		go func(k int) {
+			fn(k)
+			if atomic.AddInt32(&waiting, -1) == 0 {
+				for _, fn := range whendone {
+					fn()
+				}
+			}
+		}(k)
+	}
+}
